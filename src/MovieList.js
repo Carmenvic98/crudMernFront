bd3b8ef03ba30162b0cbb48ff547acc037e82f26
Carmenvic98 <carmenv.rios.cr@gmail.com>
2023-02-24 14:44:42 -0500

    arreglos en MovieList

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Movie from './Movie';
 
 function MovieList(){
-    return(
-        <div>
-            <h2>Lista de Peliculas</h2>
-            <Movie/>
-        </div>
-    )
+    
+    const[datamovies,setdatamovie] = useState([])
+
+    useEffect(() => {
+        axios.get('api/movie/getmovie').then(res =>{
+            console.log(res.data)
+            setdatamovie(res.data)
+        }).catch(err =>{
+            console.log(err)})
+        },[])
 }
 
 export default MovieList;
